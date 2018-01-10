2c948a34208d4198ea2643db4e7474d4f96cb9ea
Heonseok Ha <heonseok.ha@gmail.com>
2018-01-10 11:44:20 +0900

    remove simpleEnvironment

--- DQN/environment.py
+++ DQN/environment.py
@@ -4,32 +4,9 @@ import numpy as np
 import random
 import copy
 
-class Environment(object):
-    def __init__(self, args):
-        self.args = args
-
-        
-class SimpleEnvironment(Environment):
-    def __init__(self, args):
-        super(SimpleEnvironment, self).__init__(args)
-        self.env = gym.make(self.args.env_name)
-        self.num_actions = self.env.action_space.n
-        self.state_shape = list(self.env.observation_space.shape)
-
-    def new_episode(self):
-        return self.env.reset()
-
-    def act(self, action):
-        self.state, self.reward, self.terminal, _ = self.env.step(action)
-
-        return self.state, self.reward, self.terminal
-
-    def random_action(self):
-        return self.env.action_space.sample()
-
 class DKVMNEnvironment(Environment):
     def __init__(self, args, sess, dkvmn, logger):
-        super(DKVMNEnvironment, self).__init__(args)
+        self.args = args
 
         self.logger = logger 
         self.sess = sess
