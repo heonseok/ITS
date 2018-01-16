09f014659ab43c3d7befa040f2ce7c2554e1d7c5
Heonseok Ha <heonseok.ha@gmail.com>
2018-01-16 16:36:48 +0900

    update data_info.py

--- DKVMN/data_info.py
+++ DKVMN/data_info.py
@@ -18,6 +18,9 @@ class DATA_Analyzer():
         net_correct_rate_list = list()
         correct_rate_list = list()
 
+        answer_neg2pos_list = list()
+        answer_pos2neg_list = list()
+
         for lineid, line in enumerate(f_data):
             # strip
             line = line.strip()
@@ -39,47 +42,71 @@ class DATA_Analyzer():
                 net_correct_arr = np.zeros(self.n_questions)
                 correct_counter = 0
                 answer_neg2pos = np.zeros(self.n_questions, dtype=np.int)
+                answer_pos2neg = np.zeros(self.n_questions, dtype=np.int)
                 
                 for idx, answer in enumerate(answer_list):
                     answer = eval(answer)
+                    q_idx = eval(q_tag_list[idx])
+
                     if answer == 1:
-                        q_idx = eval(q_tag_list[idx])
                         net_correct_arr[q_idx-1] = 1 
                         correct_counter += 1
 
+                        answer_pos2neg[q_idx-1] = -1 
+
+                        if answer_neg2pos[q_idx-1] == -1:
+                            answer_neg2pos[q_idx-1] = 1 
+
                     elif answer == 0:
-                        answer
+                        answer_neg2pos[q_idx-1] = -1
+
+                        if answer_pos2neg[q_idx-1] == -1:
+                            answer_pos2neg[q_idx-1] = 1
                 
                 correct_rate = correct_counter/len(q_tag_list)
                 correct_rate_list.append(correct_rate)
                 
                 net_correct_rate = np.sum(net_correct_arr)/len(set(q_tag_list))
                 net_correct_rate_list.append(net_correct_rate)
-                    
+                
+                answer_neg2pos_filtered = answer_neg2pos[answer_neg2pos == 1]
+                answer_neg2pos_list.append(np.sum(answer_neg2pos_filtered))
+                #answer_neg2pos_list.append(np.sum(answer_neg2pos))
+
+                answer_pos2neg_filtered = answer_pos2neg[answer_pos2neg == 1]
+                answer_pos2neg_list.append(np.sum(answer_pos2neg_filtered))
+                #answer_pos2neg_list.append(np.sum(answer_pos2neg))
         
+        print('Total access array')
+        for idx in range(self.n_questions):
+            print('{} : {}'.format(idx+1, total_access_arr[idx]))
+        print(np.sum(total_access_arr))
+
         print('Average sequence len')
         print(np.average(sequence_len_list))
 
         print('Average correct rate')
         print(np.average(correct_rate_list))
-        print(correct_rate_list)
+        #print(correct_rate_list)
 
         print('Average net correct rate')
         print(np.average(net_correct_rate_list))
-        print(net_correct_rate_list)
+        #print(net_correct_rate_list)
 
-        print('Total access array')
-        for idx in range(self.n_questions):
-            print('{} : {}'.format(idx+1, total_access_arr[idx]))
+        print('Average neg2pos count')
+        print(np.average(answer_neg2pos_list))
+        #print(answer_neg2pos_list)
 
-        print(np.sum(total_access_arr))
+        print('Average pos2neg count')
+        print(np.average(answer_pos2neg_list))
+        #print(answer_pos2neg_list)
             
 
 if __name__ == "__main__":
     seperate_char = ','
     n_questions = 110
-    #path = 'data/assist2009_updated/assist2009_updated_train_total'
-    path = 'data/assist2009_updated/assist2009_updated_train_toy'
+    path = 'data/assist2009_updated/assist2009_updated_train_total'
+    #path = 'data/assist2009_updated/assist2009_updated_train_toy'
     #path = 'data/assist2009_updated/assist2009_updated_test'
     da = DATA_Analyzer(path, n_questions, seperate_char)
     da.analysis_dataset()
