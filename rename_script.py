aef41016765836bad2fd5bb0814a216129b84f6f
Heonseok Ha <heonseok.ha@gmail.com>
2018-09-18 05:38:55 +0900

    add rename_script for ckpt

---
+++ rename_script.py
@@ -0,0 +1,19 @@
+import numpy as np
+import os, sys
+
+# mypath = 'synthetic'
+mypath = 'assist2009_updated'
+
+from os import listdir
+from os.path import isfile, join
+print(listdir(mypath))
+
+for name in listdir(mypath):
+    print(name)
+    parts = name.split('_')
+    print(parts)
+
+    if len(parts) > 10:
+        new_name = 'knowledge_{}_niLoss_{}_rIdx_{}'.format(parts[1], parts[10], parts[14])
+        print(new_name)
+        os.rename(os.path.join(mypath,name), os.path.join(mypath,new_name))
